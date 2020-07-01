4a53e2b99b85a2b069a78d9315558c6f80ef765c
Krijn van der Raadt <krijnr@gmail.com>
2020-07-01 16:32:35 -0700

    complete issue-2 (Save Image Link) with all acceptance criteria

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,3 @@
 class Image < ApplicationRecord
-    validates :path, presence: true
+  validates :path, presence: true, format: URI.regexp(%w[http https])
 end
--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -2,11 +2,19 @@ require 'test_helper'
 
 class ImageTest < ActiveSupport::TestCase
   test 'create image' do
-    image = Image.create path:'/not/a/real/image'
+    image = Image.create path: 'http://www.nope.com/not/a/real/image/image.png'
     assert image.valid?
   end
   test 'create invalid image' do
-    image = Image.create 
+    image = Image.create
+    assert image.invalid?
+  end
+  test 'create image with invalid URI scheme' do
+    image = Image.create path: 'ftp://not/a/url'
+    assert image.invalid?
+  end
+  test 'create image with incomplete URL' do
+    image = Image.create path: 'image.jpg'
     assert image.invalid?
   end
 end
