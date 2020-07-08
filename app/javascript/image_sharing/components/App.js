3ce09583c6b038faedae703d26deae9cb39f1d81
Krijn van der Raadt <krijnr@gmail.com>
2020-07-08 15:09:53 -0700

    KvdR - Add feedback to App

--- app/javascript/image_sharing/components/App.js
+++ app/javascript/image_sharing/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header';
+import Feedback from './Feedback';
 
 export default function App() {
   return (
     <div>
       <Header title="Tell us what you think" />
-      Put your components here: Flash Message, Form, Footer
+      <Feedback />
+      <footer className="text-center">Copyright: AppFolio</footer>
     </div>
   );
 }
--- app/javascript/image_sharing/test/components/App.spec.js
+++ app/javascript/image_sharing/test/components/App.spec.js
@@ -4,10 +4,15 @@ import assert from 'assert';
 import { mount } from 'enzyme';
 import React from 'react';
 import App from '../../components/App';
+import Feedback from '../../components/Feedback';
 
 describe('<App />', () => {
   it('should render correctly', () => {
     const wrapper = mount(<App />);
     assert(wrapper.contains('Tell us what you think'));
+    assert(wrapper.exists(Feedback));
+    const footer = wrapper.find('footer');
+    assert(footer.exists(), 'footer should exist');
+    assert.strictEqual(footer.text(), 'Copyright: AppFolio');
   });
 });
